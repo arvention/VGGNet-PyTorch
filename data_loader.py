bf7f82826c4f6fac27ce7a88ca1bc6e22cd717c1
arvention <amc.antioquia@yahoo.com.ph>
2018-08-13 18:39:22 +0800

    finished Caltech256Dataset in data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,17 +1,28 @@
-
+import h5py
+import torch
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
 
 
-class PascalVOC(Dataset):
+class Caltech256Dataset(Dataset):
 
-    def __int__(self):
+    def __int__(self, data_path, x_key, y_key):
         """
         Initialize dataset
         """
 
-        # TODO: get data
-        # TODO: transform data
-        # TODO: set self.N
+        # get data
+        data_file = h5py.File(data_path, 'r')
+        self.x = data_file[x_key]
+        self.y = data_file[y_key]
+        self.N = self.x.shape[0]
+
+        # transform data
+        self.transform = transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+        )
 
     def __len__(self):
         """
@@ -23,11 +34,23 @@ class PascalVOC(Dataset):
         """
         Return item from dataset
         """
-        pass
+        image = self.x[index]
+        label = self.y[index]
+        return self.transform(image), torch.from_numpy(label).long()
 
 
-def get_loader(data_path, mode='train'):
+def get_loader(data_path, x_key, y_key, batch_size, mode='train'):
     """
     Get dataset loader
     """
-    pass
+    dataset = Caltech256Dataset(data_path, x_key, y_key)
+
+    shuffle = False
+    if mode == 'train':
+        shuffle = True
+
+    data_loader = DataLoader(dataset=dataset,
+                             batch_size=batch_size,
+                             shuffle=shuffle)
+
+    return data_loader
