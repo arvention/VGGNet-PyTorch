d18794dabab9ac29614e42912d434c341b633bc6
arvention <amc.antioquia@yahoo.com.ph>
2018-08-14 21:16:32 +0800

    finished solver.py

--- solver.py
+++ solver.py
@@ -2,6 +2,11 @@ import os
 import torch
 import time
 import datetime
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+from utils import to_var
+
 from model import VGGNet
 
 
@@ -35,16 +40,22 @@ class Solver(object):
         # instantiate VGGNet model
         self.model = VGGNet(self.input_channels, self.class_count)
 
-        # TODO: instantiate loss criterion
+        # instantiate loss criterion
+        self.criterion = nn.CrossEntropyLoss()
 
         # TODO: instantiate optimizer
+        self.optimizer = optim.SGD(
+            self.model.parameters(),
+            lr=self.lr,
+            momentum=self.momentum
+        )
 
         # print network
         self.print_network(self.model, 'VGGNet')
 
         if torch.cuda.is_available() and self.use_gpu:
             self.model.cuda()
-            # TODO: set criterion to cuda
+            self.criterion.cuda()
 
     def print_network(self, model, name):
         """
@@ -110,8 +121,41 @@ class Solver(object):
         """
         Training process
         """
-        # TODO: add training process
-        pass
+        self.losses = []
+        self.top_1_acc = []
+        self.top_5_acc = []
+
+        iters_per_epoch = len(self.data_loader)
+
+        # start with a trained model if exists
+        if self.pretrained_model:
+            start = int(self.pretrained_model.split('_')[0])
+        else:
+            start = 0
+
+        # start training
+        start_time = time.time()
+        for e in range(start, self.num_epochs):
+            for i, (images, labels) in enumerate(tqdm(self.data_loader)):
+                images = to_var(images, self.use_gpu)
+                labels = to_var(images, self.use_gpu)
+
+                loss = self.model_step(images, labels)
+
+            # print out loss log
+            if (e + 1) % self.loss_log_step == 0:
+                self.print_loss_log(start_time, iters_per_epoch, e, i, loss)
+                self.losses.append((e, loss))
+
+            # save model
+            if (e + 1) % self.model_save_step == 0:
+                self.save_model(e, i)
+
+            # evaluate on train dataset
+            if (e + 1) % self.train_eval_step == 0:
+                top_1_acc, top_5top_1_acc = self.train_evaluate(e)
+                self.top_1_acc.append((e, top_1_acc))
+                self.top_5_acc.append((e, top_5top_1_acc))
 
     def model_step(self, images, labels):
         """
@@ -122,30 +166,44 @@ class Solver(object):
         self.model.train()
 
         # empty the gradients of the model through the optimizer
-        # TODO: self.optimizer.zero_grad()
+        self.optimizer.zero_grad()
 
         # forward pass
-        # TODO: output = self.model(images)
+        output = self.model(images)
 
         # compute loss
-        # TODO: self.criterion(output, labels.squeeze())
+        loss = self.criterion(output, labels.squeeze())
 
         # compute gradients using back propagation
-        # TODO: loss.backward()
+        loss.backward()
 
         # update parameters
-        # TODO: self.optimizer.step()
+        self.optimizer.step()
 
         # return loss
-        # TODO: return loss
-        pass
+        return loss
+
+    def train_evaluate(self, e):
+        top_1_correct, top_5_correct, total = self.eval(self.data_loader)
+        log = "Epoch [{}/{}]--top_1_acc: {:.4f}--top_5_acc: {:.4f}".format(
+            e + 1,
+            self.num_epochs,
+            top_1_correct / total,
+            top_5_correct / total
+        )
+        print(log)
+        return top_1_correct / total, top_5_correct / total
 
     def test(self):
         """
         Evaluates the performance of the model using a test dataset
         """
-        # TODO: call self.eval() then print log
-        pass
+        top_1_correct, top_5_correct, total = self.eval(self.data_loader)
+        log = "top_1_acc: {:.4f}--top_5_acc: {:.4f}".format(
+            top_1_correct / total,
+            top_5_correct / total
+        )
+        print(log)
 
     def eval(self, data_loader):
         """
@@ -154,4 +212,29 @@ class Solver(object):
 
         self.model.eval()
 
-        # TODO: return mAP
+        top_1_correct = 0
+        top_5_correct = 0
+        total = 0
+
+        for images, labels in data_loader:
+
+            images = to_var(images, self.use_gpu)
+            labels = to_var(labels, self.use_gpu)
+
+            output = self.model(images)
+            total += labels.size()[0]
+
+            # top 1
+            # get the max for each instance in the batch
+            _, top_1_output = torch.max(output.data, dim=1)
+
+            top_1_correct += torch.sum(torch.eq(labels.squeeze(),
+                                                top_1_output))
+
+            # top 5
+            _, top_5_output = torch.topk(output.data, k=5, dim=1)
+            for i, label in enumerate(labels):
+                if label in top_5_output[i]:
+                    top_5_correct += 1
+
+        return top_1_correct.item(), top_5_correct, total
