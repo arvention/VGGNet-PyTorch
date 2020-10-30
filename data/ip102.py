e17b6fccde892da94fb64a93bd89e05c4f692d4d
Arren Matthew C. Antioquia <amc.antioquia@yahoo.com.ph>
2020-10-31 02:49:10 +0800

    added ip102.py file

---
+++ data/ip102.py
@@ -0,0 +1,121 @@
+import os.path as osp
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+import cv2
+
+
+class IP102(Dataset):
+
+    def __init__(self,
+                 data_path,
+                 mode,
+                 new_size):
+        """Initializes the dataset
+
+        Arguments:
+            data_path {string} -- root path to the IP102 dataset
+            mode {string} -- current mode of the network
+            new_size {int} -- rescaled size of the image
+        """
+
+        self.data_path = data_path
+        self.mode = mode
+        self.new_size = new_size
+
+        self.ids = []
+        self.targets = []
+
+        self.image_path = osp.join(self.data_path, 'images', '%s')
+
+        list_path = ''
+        if self.mode == 'train':
+            list_path = osp.join(self.data_path, 'train.txt')
+        elif self.mode == 'val':
+            list_path = osp.join(self.data_path, 'val.txt')
+        elif self.mode == 'test':
+            list_path = osp.join(self.data_path, 'test.txt')
+
+        with open(list_path) as f:
+            for line in f:
+                values = line.split(' ')
+                self.ids.append(values[0])
+                self.targets.append(values[1])
+
+    def __len__(self):
+        """Returns number of data in the dataset
+
+        Returns:
+            int -- number of data in the dataset
+        """
+        return len(self.ids)
+
+    def __getitem__(self, index):
+        """Returns an image and its corresponding class from the dataset
+
+        Arguments:
+            index {int} -- index of the item to be pulled from the list of ids
+
+        Returns:
+            torch.Tensor, string -- Tensor representation of the pulled image
+            and string representation of the class of the image
+        """
+        image, target, _, _ = self.pull_item(index)
+
+        return image, target
+
+    def pull_item(self, index):
+        """Returns an image, its corresponding class, height, and width from
+        the dataset
+
+        Arguments:
+            index {int} -- index of the item to be pulled from the list of ids
+
+        Returns:
+            torch.Tensor, string, int, int -- Tensor representation of the
+            pulled image, string representation of the class of the image,
+            height of the image, width of the image
+        """
+        image_id = self.ids[index]
+
+        image = cv2.imread(self.image_path % image_id)
+        target = self.targets[index]
+        height, width, _ = image.shape
+
+        return torch.from_numpy(image).permute(2, 0, 1), target, height, width
+
+    def pull_image(self, index):
+        """Returns an image from the dataset represented as an ndarray
+
+        Arguments:
+            index {int} -- index of the item to be pulled from the list of ids
+
+        Returns:
+            numpy.ndarray -- ndarray representation of the pulled image
+        """
+
+        image_id = self.ids[index]
+        return cv2.imread(self.image_path % image_id, cv2.IMREAD_COLOR)
+
+    def pull_target(self, index):
+        """Returns a class corresponding to an image from the dataset
+
+        Arguments:
+            index {int} -- index of the item to be pulled from the list of ids
+
+        Returns:
+            string -- string representation of the class of an image from the
+            dataset
+        """
+        return self.targets[index]
+
+    def pull_tensor(self, index):
+        """Returns an image from the dataset represented as a tensor
+
+        Arguments:
+            index {int} -- index of the item to be pulled from the list of ids
+
+        Returns:
+            torch.Tensor -- Tensor representation of the pulled image
+        """
+        return torch.Tensor(self.pull_image(index)).unsqueeze_(0)
