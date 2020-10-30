41a8acef49fab886e595386c8394b04488d856fa
Arren Matthew C. Antioquia <amc.antioquia@yahoo.com.ph>
2020-10-31 02:47:37 +0800

    transfered to_var and mkdir in utils.utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,19 @@
+import os
+import torch
+
+
+def to_var(x, use_gpu, requires_grad=False):
+    if torch.cuda.is_available() and use_gpu:
+        x = x.cuda()
+    x.requires_grad = requires_grad
+    return x
+
+
+def mkdir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
 def write_print(path, text):
     """Displays text in console and saves in text file
 
