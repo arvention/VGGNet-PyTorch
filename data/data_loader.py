93e6bd662edeb6ed793efe44d6b74991dbf27e2c
Arren Matthew C. Antioquia <amc.antioquia@yahoo.com.ph>
2020-11-16 19:12:31 +0800

    Added collate() function and validation set loader for IP102 dataset

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,6 +1,16 @@
+import torch
 from data.ip102 import IP102
 from torch.utils.data import DataLoader
 from data.augmentations import Augmentations, BaseTransform
+
+
+def collate(batch):
+    images = []
+    targets = []
+    for sample in batch:
+        images.append(sample[0])
+        targets.append(int(sample[1]))
+    return torch.stack(images, 0), targets
 
 
 def get_loader(config):
@@ -16,7 +26,14 @@ def get_loader(config):
                             new_size=config.new_size,
                             image_transform=image_transform)
 
-        if config.mode == 'test':
+        elif config.mode == 'val':
+            image_transform = BaseTransform(config.new_size, config.means)
+            dataset = IP102(data_path=config.ip102_data_path,
+                            mode='val',
+                            new_size=config.new_size,
+                            image_transform=image_transform)
+
+        elif config.mode == 'test':
             image_transform = BaseTransform(config.new_size, config.means)
             dataset = IP102(data_path=config.ip102_data_path,
                             mode='test',
@@ -28,13 +45,15 @@ def get_loader(config):
             loader = DataLoader(dataset=dataset,
                                 batch_size=config.batch_size,
                                 shuffle=True,
+                                collate_fn=collate,
                                 num_workers=8,
                                 pin_memory=True)
 
-        elif config.mode == 'test':
+        elif config.mode == 'val' or config.mode == 'test':
             loader = DataLoader(dataset=dataset,
                                 batch_size=config.batch_size,
                                 shuffle=False,
+                                collate_fn=collate,
                                 num_workers=8,
                                 pin_memory=True)
 
